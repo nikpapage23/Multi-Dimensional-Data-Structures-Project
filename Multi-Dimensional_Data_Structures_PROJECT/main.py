6a58540540b18fae14eefb72f2281a123f925cf2
Alexandros Xiarchos <36487552+alex-xiarchos@users.noreply.github.com>
2023-09-13 15:17:33 +0300

    add support for lowercase input & add switch 1

--- Multi-Dimensional_Data_Structures_PROJECT/main.py
+++ Multi-Dimensional_Data_Structures_PROJECT/main.py
@@ -1,9 +1,10 @@
 from r_tree import *
-from range_tree import *
+from k_d_tree import *
 from beautifultable import BeautifulTable
-import time
 import warnings
+
 warnings.filterwarnings("ignore", category=FutureWarning)
+import time
 
 
 def display_results(results_list):
@@ -24,25 +25,33 @@ if __name__ == '__main__':
     min_letter, max_letter = input("Εισάγετε διάστημα ονομάτων στη μορφή X,X: ").split(',')
     num_awards = int(input("Εισάγετε ελάχιστο αριθμό βραβείων: "))
 
+    min_letter = min_letter.upper()
+    max_letter = max_letter.upper()
+
     # Επιλογή πολυδιάστατης δομής για αποθήκευση των δεδομένων
     print("\n1. k-d tree\n2. Quad tree\n3. Range tree\n4. R-tree")
     choice = int(input("Επιλέξτε δομή: "))
 
     start_time = time.time()
 
-    if choice == 1:     # Δομή k-d tree
+    if choice == 1:  # Δομή k-d tree
+        [kdtree, points_xy, datamap] = build_kdtree()
+        results = query_kdtree(kdtree, points_xy, min_letter, max_letter, num_awards)
+        # display_results(results)
+
+    elif choice == 2:  # Δομή Quad tree
         pass
-    elif choice == 2:   # Δομή Quad tree
+
+    elif choice == 3:  # Δομή Range tree
+        # range_tree = build_range_tree()
+        # results = query_range_tree(range_tree, min_letter, max_letter, num_awards)
+        # display_results(results)
         pass
-    elif choice == 3:   # Δομή Range tree
-        range_tree = build_range_tree()
-        results = query_range_tree(range_tree, min_letter, max_letter, num_awards)
-        display_results(results)
-    elif choice == 4:   # Δομή R-tree
+
+    elif choice == 4:  # Δομή R-tree
         rtree = build_rtree()
         results = query_rtree(rtree, min_letter, max_letter, num_awards)
         display_results(results)
 
     end_time = time.time()
     print(end_time - start_time)
-
