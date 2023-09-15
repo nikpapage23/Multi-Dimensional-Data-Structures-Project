3e46f447fd32eb85f00f06a71b1b81ef3077a1d7
nikpapage23 <65818389+nikpapage23@users.noreply.github.com>
2023-09-15 10:59:35 +0300

    moved main's functionality to a function

--- Multi-Dimensional_Data_Structures_PROJECT/main.py
+++ Multi-Dimensional_Data_Structures_PROJECT/main.py
@@ -1,7 +1,7 @@
 from r_tree import *
 from range_tree import *
+from quad_tree import *
 from beautifultable import BeautifulTable
-import time
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
@@ -18,16 +18,38 @@ def display_results(results_list):
     print(table)
 
 
+def main_app(lsh_threshold, min_letter, max_letter, num_awards, choice):
+    if choice == 1:     # Δομή k-d tree
+        pass
+    elif choice == 2:   # Δομή Quad tree
+        quad_tree = build_quad_tree()
+        results = query_quad_tree(quad_tree, min_letter, max_letter, num_awards)
+        display_results(results)
+    elif choice == 3:   # Δομή Range tree
+        range_tree = build_range_tree()
+        results = query_range_tree(range_tree, min_letter, max_letter, num_awards)
+        display_results(results)
+    elif choice == 4:   # Δομή R-tree
+        rtree = build_rtree()
+        results = query_rtree(rtree, min_letter, max_letter, num_awards)
+        display_results(results)
+
+    # ~ LSH CODE ~
+
+
 if __name__ == '__main__':
     # Εισαγωγή των απαιτούμενων πληροφοριών αναζήτησης από τον χρήστη
-    lsh_threshold = float(input("Εισάγετε ελάχιστο ποσοστό ομοιότητας (0 - 1): "))
-    min_letter, max_letter = input("Εισάγετε διάστημα ονομάτων στη μορφή X,X: ").split(',')
-    num_awards = int(input("Εισάγετε ελάχιστο αριθμό βραβείων: "))
+    thresh = float(input("Εισάγετε ελάχιστο ποσοστό ομοιότητας (0 - 1): "))
+    letter1, letter2 = input("Εισάγετε διάστημα ονομάτων στη μορφή X,X: ").split(',')
+    awards = int(input("Εισάγετε ελάχιστο αριθμό βραβείων: "))
 
     # Επιλογή πολυδιάστατης δομής για αποθήκευση των δεδομένων
     print("\n1. k-d tree\n2. Quad tree\n3. Range tree\n4. R-tree")
-    choice = int(input("Επιλέξτε δομή: "))
+    user_input = int(input("Επιλέξτε δομή: "))
 
+    main_app(thresh, letter1, letter2, awards, user_input)
+
+    '''
     start_time = time.time()
 
     if choice == 1:     # Δομή k-d tree
@@ -43,6 +65,9 @@ if __name__ == '__main__':
         results = query_rtree(rtree, min_letter, max_letter, num_awards)
         display_results(results)
 
+    # LSH
+
     end_time = time.time()
     print(end_time - start_time)
+    '''
 
